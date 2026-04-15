5aae2a7049ccee5013f5930287efd158f467705a
Opilane TTHK <opilane@tthk.ee>
2026-04-15 16:39:13 +0300

    lisan alguse

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import ToDoItem from './ToDoItem'
+
+type ToDo = {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+
+//function App() {
+
+
+  return (
+    <div className="app">
+      
+    </div>
+  );
+};
+
+
+export default App
